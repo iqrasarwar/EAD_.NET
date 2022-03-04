0f2d057e0fcdcf8b412a9adb1041a5ce272ec69a
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-03-04 22:48:43 +0500

    Add files via upload

--- HomeWorks/HomeWork#01/Program.cs
+++ HomeWorks/HomeWork#01/Program.cs
@@ -1,1 +1,22 @@
-
+﻿using System;
+using PresenatationLayer;
+namespace SemesterVI
+{
+    class Program
+    {
+        static void Main(string[] args)     
+        {
+            EmpView v = new EmpView();
+            Console.WriteLine("\t\tStoring New Emplooyes to File");
+            string choice = "";
+            do
+            {
+                v.InputEmpData();
+                Console.Write("\nWanna Enter next Object(1/0): ");
+                choice = Console.ReadLine();
+            } while (choice == "1");
+            Console.WriteLine("\t\tReading Emplooyes from File");
+            v.DisplayEmpData();
+        }
+    }
+}
---
+++ HomeWorks/HomeWork#01/ReadMe.txt
@@ -0,0 +1,5 @@
+EmpView => presentation layer
+EmpBussLaogic => bussiness logic layer
+EmpDAL => data access layer
+EmpBussinessObjs => bussiness objects
+Program => main/driver
