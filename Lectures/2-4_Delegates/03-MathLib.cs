bb38bc981927e228029805e962f8907c79936f92
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-03-23 11:41:08 +0500

    Update 03-MathLib.cs

--- Lectures/2-4_Delegates/03-MathLib.cs
+++ Lectures/2-4_Delegates/03-MathLib.cs
@@ -1,9 +1,7 @@
-using System;
-
 namespace MathLib
 {
     public delegate int Mathop(int x, int y);
-    public class Class1
+    public class MathLibrary
     {
         public int add(int a,int b)
         {
@@ -21,6 +19,30 @@ namespace MathLib
             else if (input == 2)
                 op = this.sub;
             return op;
+        }
+    }
+}
+
+using System;
+using MathLib;
+delegate void MyDelegate();
+namespace SemesterVI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate d1 = delegate {
+                Console.WriteLine("delegate in global space");
+            };
+            d1();
+            MathLibrary lib = new MathLibrary();
+            Console.WriteLine("enter input");
+            int input = int.Parse(Console.ReadLine());
+            var d = lib.GetFun(input);
+            Console.WriteLine(d(1, 2));
+            Console.WriteLine(lib.add(2, 3));
+            Console.WriteLine(lib.sub(2, 3));
         }
     }
 }
