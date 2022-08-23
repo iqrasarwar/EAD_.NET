02e50f7e4195496b12866862f78e16be7aff22e7
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-08-23 22:11:05 +0500

    dependency injection and file uploading

---
+++ Lectures/POSTMID/02/file uploading/Models/Repostries/IEmployeeRepositry.cs
@@ -0,0 +1,21 @@
+﻿using Dependency_Injection.Models.Interfaces;
+
+namespace Dependency_Injection.Models.Repostries
+{
+    public class IEmployeeRepositry : IEmployee
+    {
+        public List<Employee> GetAllEmployee()
+        {
+            Employee[] employees = new Employee[10];
+            List<Employee> employeeList = new List<Employee>();
+            int index = 0;
+            foreach (Employee emp in employees)
+            {
+                emp.id = index++;
+                emp.Name = "iqra";
+                employeeList.Add(emp);
+            }
+            return employeeList;
+        }
+    }
+}
