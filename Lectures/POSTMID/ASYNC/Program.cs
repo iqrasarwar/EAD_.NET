d4d5684935b2891907e41c40be3e352517894453
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-09-19 19:11:32 +0500

    Add files via upload

---
+++ Lectures/POSTMID/ASYNC/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading;
+using System.Diagnostics;
+namespace ASYNC
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var timer = Stopwatch.StartNew();
+         Console.WriteLine("Hello World!");
+         // A();
+         // B();
+         // C();
+         Task a = asynA();
+         Task b = asyncB();
+         Task c = asynC();
+         Task[] tasks = { a, b, c };
+         Task.WaitAll(tasks);
+         Console.Write(timer.ElapsedMilliseconds);
+      }
+
+      static void A()
+      {
+         Console.WriteLine("Before A");
+         Thread.Sleep(3000);
+         Console.WriteLine("After A");
+      }
+      static void B()
+      {
+         Console.WriteLine("Before B");
+         Thread.Sleep(3000);
+         Console.WriteLine("After B");
+      }
+      static void C()
+      {
+         Console.WriteLine("Before C");
+         Thread.Sleep(3000);
+         Console.WriteLine("After C");
+      }
+
+      static async Task asynA()
+      {
+         Console.WriteLine("Before A");
+         await Task.Delay(3000);
+         Console.WriteLine("After A");
+      }
+      static async Task asyncB()
+      {
+         Console.WriteLine("Before b");
+         await Task.Delay(3000);
+         Console.WriteLine("After b");
+      }
+      static async Task asynC()
+      {
+         Console.WriteLine("Before c");
+         await Task.Delay(3000);
+         Console.WriteLine("After c");
+      }
+   }
+}
